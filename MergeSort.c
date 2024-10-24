26f9fe5c5dbc6bea87c6ba0063cb0500afb727c1
eidiyoshi <eidiyoshi@hotmail.com>
2024-10-24 14:32:07 -0300

    MergeSort added

---
+++ MergeSort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int array[], int inicio, int meio, int tamanho)
+{
+    int i, j, k;
+    int n1 = meio - inicio + 1;
+    int n2 = tamanho - meio;
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = array[inicio + i];
+    for (j = 0; j < n2; j++)
+        R[j] = array[meio + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = inicio;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            array[k] = L[i];
+            i++;
+        }
+        else {
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l = inicio, r = tamanho
+void MergeSort(int array[], int inicio, int tamanho)
+{
+    if (inicio < tamanho) {
+        int meio = inicio + (tamanho - inicio) / 2;
+
+        // Sort first and second halves
+        MergeSort(array, inicio, meio);
+        MergeSort(array, meio + 1, tamanho);
+
+        merge(array, inicio, meio, tamanho);
+    }
+}
---
+++ MergeSort.h
@@ -0,0 +1,5 @@
+#ifndef MERGESORT_
+#define MERGESORT_
+#include "MergeSort.c"
+    void MergeSort(int array[], int start ,int size);
+#endif
